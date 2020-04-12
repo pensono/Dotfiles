16247098375a37f8e1e701bf13d4bbf6bda07506
Ethan Shea <ethan.shea1@gmail.com>
2020-04-11 21:33:48 -0700

    Remove warning producing line

--- windows/Setup.ps1
+++ windows/Setup.ps1
@@ -4,9 +4,8 @@ function Write-Progress($message) {
 
 
 function Install-Prerequisites() {
-    Set-ExecutionPolicy Bypass -Scope Process -Force
-     [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
-     iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+    iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 }
 
 function Setup-Explorer() {
