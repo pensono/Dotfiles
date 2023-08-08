1fe70955037ae450ad2afa198ab88789baf02f4b
Ethan Shea <ethan.shea1@gmail.com>
2023-08-08 15:16:36 -0400

    Add reference for capslock rebinding

--- mac/setup.sh
+++ mac/setup.sh
@@ -1,2 +1,3 @@
 # Rebind capslock to backspace
+# https://apple.stackexchange.com/a/324832
 hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000039,"HIDKeyboardModifierMappingDst":0x70000002A}]}'
