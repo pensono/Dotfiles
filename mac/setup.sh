27e1dba8bd53235cdbb78c68f72542270da65a07
Ethan Shea <ethan.shea1@gmail.com>
2023-08-15 15:53:29 -0400

    Combine keyboard mappings

--- mac/setup.sh
+++ mac/setup.sh
@@ -1,6 +1,3 @@
-# Rebind capslock to backspace
 # https://apple.stackexchange.com/a/324832
-hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000039,"HIDKeyboardModifierMappingDst":0x70000002A}]}'
-
-# Right shift to shift-delete
-hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x7000000E5,"HIDKeyboardModifierMappingDst":0x70000004C}]}'
+# Rebind capslock to backspace and right shift to shift-delete
+hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000039,"HIDKeyboardModifierMappingDst":0x70000002A},{"HIDKeyboardModifierMappingSrc":0x7000000E5,"HIDKeyboardModifierMappingDst":0x70000004C}]}'
