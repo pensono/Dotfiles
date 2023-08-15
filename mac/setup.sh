1b9b78af74c9584a18c23d1e4f5dbd1f55465c51
Ethan Shea <ethan.shea1@gmail.com>
2023-08-15 15:52:18 -0400

    Add shift -> delete keymapping

--- mac/setup.sh
+++ mac/setup.sh
@@ -1,3 +1,6 @@
 # Rebind capslock to backspace
 # https://apple.stackexchange.com/a/324832
 hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000039,"HIDKeyboardModifierMappingDst":0x70000002A}]}'
+
+# Right shift to shift-delete
+hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x7000000E5,"HIDKeyboardModifierMappingDst":0x70000004C}]}'
