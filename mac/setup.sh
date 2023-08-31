57e4d6f0dcc2503e4483b40b8bde042cb5dfcbab
Ethan Shea <ethan.shea1@gmail.com>
2023-08-30 22:42:05 -0400

    Configure git editor

--- mac/setup.sh
+++ mac/setup.sh
@@ -1,3 +1,6 @@
 # https://apple.stackexchange.com/a/324832
 # Rebind capslock to backspace and right shift to shift-delete
 hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000039,"HIDKeyboardModifierMappingDst":0x70000002A},{"HIDKeyboardModifierMappingSrc":0x7000000E5,"HIDKeyboardModifierMappingDst":0x70000004C}]}'
+
+# Pico/nano in tool mode (disables autosave)
+git config --global core.editor "pico -t"
