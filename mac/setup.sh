49303a4212e771d91afa805657b89300cc1df1e0
Ethan Shea <142333519+ethshea@users.noreply.github.com>
2023-10-31 10:58:21 -0400

    Use correct path for keyboard remap

--- mac/setup.sh
+++ mac/setup.sh
@@ -1,7 +1,7 @@
 # https://apple.stackexchange.com/a/324832
 # Rebind capslock to backspace and right shift to shift-delete
 mkdir ~/Library/LaunchAgent
-cp keyboardremap.plist ~/Library/LaunchAgent
+cp $(dirname "$0")/keyboardremap.plist ~/Library/LaunchAgent
 chmod 600 ~/Library/LaunchAgents/keyboardremap.plist
 launchctl load ~/Library/LaunchAgents/keyboardremap.plist
 
