a45b124151a8f0e77cdac1a7279779e5055e15f9
Ethan Shea <142333519+ethshea@users.noreply.github.com>
2023-10-31 10:56:22 -0400

    Add keyboardremap to launchctl

--- mac/setup.sh
+++ mac/setup.sh
@@ -1,5 +1,8 @@
 # https://apple.stackexchange.com/a/324832
 # Rebind capslock to backspace and right shift to shift-delete
-hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000039,"HIDKeyboardModifierMappingDst":0x70000002A},{"HIDKeyboardModifierMappingSrc":0x7000000E5,"HIDKeyboardModifierMappingDst":0x70000004C}]}'
+mkdir ~/Library/LaunchAgent
+cp keyboardremap.plist ~/Library/LaunchAgent
+chmod 600 ~/Library/LaunchAgents/keyboardremap.plist
+launchctl load ~/Library/LaunchAgents/keyboardremap.plist
 
 $(dirname "$0")/../shared/git_setup.sh
