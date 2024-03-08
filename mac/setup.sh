caadcfaaf338a924eb30323bfa71745df2959e7f
Ethan Shea <ethan.shea@vercel.com>
2024-03-08 10:57:29 -0500

    Update setup.sh

--- mac/setup.sh
+++ mac/setup.sh
@@ -5,4 +5,8 @@ cp $(dirname "$0")/keyboardremap.plist ~/Library/LaunchAgent
 chmod 600 ~/Library/LaunchAgents/keyboardremap.plist
 launchctl load ~/Library/LaunchAgents/keyboardremap.plist
 
+# Home/end keys go to the end of line instead of the end of the page
+mkdir -p ~/Library/KeyBindings
+cp $(dirname "$0")/DefaultKeyBinding.dict ~/Library/KeyBindings
+
 $(dirname "$0")/../shared/git_setup.sh
