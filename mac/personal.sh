187612caf5914a1ab01a88c94d8ad91422919293
Ethan Shea <ethan.shea1@gmail.com>
2024-12-30 19:46:23 -0500

    Add more personal homebrew apps

--- mac/personal.sh
+++ mac/personal.sh
@@ -1,6 +1,6 @@
 set -e
 
-brew install --cask steam bitwarden
+brew install --cask steam bitwarden notion telegram
 
 git config --global user.name "Ethan Shea"
 git config --global user.email "ethan.shea1@gmail.com"
