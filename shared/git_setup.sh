7477787816fcd94f8ae6ea5f225011cc62ad2c93
Ethan Shea <ethan.shea1@gmail.com>
2025-01-09 18:00:57 +0100

    Better log format

--- shared/git_setup.sh
+++ shared/git_setup.sh
@@ -1,5 +1,5 @@
-# Pico/nano in tool mode (disables autosave)
-git config --global core.editor "pico -t"
+git config --global log.date relative
+git config --global format.pretty "format:%C(yellow)%h %Cblue%>(12)%ad %Cgreen%<(7)%aN %Creset%s%Cred%d"
 
 # Show recent branches
 git config --global alias.lb "!git for-each-ref --sort=-committerdate refs/heads/ --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(color:red)%(objectname:short)%(color:reset) - %(contents:subject) - %(authorname) (%(color:green)%(committerdate:relative)%(color:reset))' --count=\${1:-15}"
