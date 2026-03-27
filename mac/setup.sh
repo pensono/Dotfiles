9126f47e772ef01bdc9234880d2344446dd59429
Ethan Shea <ethan.shea1@gmail.com>
2026-03-26 19:55:54 -0700

    Modify setup.sh to include 'cursor' app and delta config
    Updated setup script to install 'cursor' and ensure delta is configured for git diff.

--- mac/setup.sh
+++ mac/setup.sh
@@ -1,10 +1,5 @@
 set -ex
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-echo >> /Users/$USER/.zprofile
-echo 'eval "$(/opt/homebrew/bin/brew shellenv zsh)"' >> /Users/$USER/.zprofile
-eval "$(/opt/homebrew/bin/brew shellenv zsh)"
-
 # https://apple.stackexchange.com/a/324832
 # Rebind capslock to backspace and right shift to shift-delete
 mkdir -p ~/Library/LaunchAgents
@@ -18,11 +13,6 @@ cp $(dirname "$0")/DefaultKeyBinding.dict ~/Library/KeyBindings
 
 $(dirname "$0")/../shared/git_setup.sh
 
-# Better git diff
-# https://x.com/rauchg/status/1831421759666676165
-brew install delta
-git config --global core.pager "delta"
-
 # Install Homebrew
 if ! [ -x "$(command -v brew)" ]; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -32,7 +22,12 @@ if ! [ -x "$(command -v brew)" ]; then
 fi
 
 # Install apps
-brew install --cask raycast visual-studio-code ghostty programmer-dvorak spotify
+brew install --cask raycast visual-studio-code ghostty programmer-dvorak spotify cursor
+
+# Better git diff
+# https://x.com/rauchg/status/1831421759666676165
+brew install delta
+git config --global core.pager "delta"
 
 if ! [ -x "$(command -v nvm)" ]; then
     # https://nodejs.org/en/download
