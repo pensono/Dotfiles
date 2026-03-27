dbe80470bde9ce7024ad17cd864be2c41ef6a1e6
Ethan Shea <ethan.shea1@gmail.com>
2026-03-26 19:51:18 -0700

    Install latest Node.js using nvm
    Replaced specific Node.js version installation with latest version.

--- mac/setup.sh
+++ mac/setup.sh
@@ -37,12 +37,14 @@ brew install --cask raycast visual-studio-code ghostty programmer-dvorak spotify
 if ! [ -x "$(command -v nvm)" ]; then
     # https://nodejs.org/en/download
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
-    nvm install 22
 
     # Post-install
     export NVM_DIR="$HOME/.nvm"
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
     [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+    # Install latest node
+    nvm install node
 fi
 
 corepack enable pnpm
